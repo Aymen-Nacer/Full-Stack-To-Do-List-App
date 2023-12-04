5b04dec1abdc4126a7d405962f983f8a8208aa87
Aymen Nacer <aymen@nacer.com>
2023-12-04 12:57:01 +0300

    Modify Task model to match the actual column names in the database

--- To-Do-List-App-SpringBoot/src/main/java/com/aymen/todolist/model/Task.java
+++ To-Do-List-App-SpringBoot/src/main/java/com/aymen/todolist/model/Task.java
@@ -2,78 +2,86 @@ package com.aymen.todolist.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
 @Entity(name="tasks")
 public class Task {
     @Id
-    private Long taskId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long task_id;
 
     @Column(nullable = false)
-    private String taskDescription;
+    private String task_description;
 
     @Column(nullable = false)
-    private boolean isComplete;
+    private boolean complete;
 
     @Temporal(TemporalType.TIMESTAMP)
     @CreationTimestamp
-    @Column(name = "created_at", updatable = false, nullable = false)
-    private Date createdAt;
+    @Column(updatable = false, nullable = false)
+    private Timestamp created_at;
 
 
-    protected Task() {}
+    public Task() {
 
-    public Task(Long taskId, String taskDescription, boolean isComplete, Date createdAt) {
-        this.taskId = taskId;
-        this.taskDescription = taskDescription;
-        this.isComplete = isComplete;
-        this.createdAt = createdAt;
+    }
+
+    public Task(Long taskId, String taskDescription, boolean isComplete, Timestamp createdAt) {
+        this.task_id = taskId;
+        this.task_description = taskDescription;
+        this.complete = isComplete;
+        this.created_at = createdAt;
     }
 
     public Long getTaskId() {
-        return taskId;
+        return task_id;
     }
 
     public void setTaskId(Long taskId) {
-        this.taskId = taskId;
+        this.task_id = taskId;
     }
 
     public String getTaskDescription() {
-        return taskDescription;
+        return task_description;
     }
 
     public void setTaskDescription(String taskDescription) {
-        this.taskDescription = taskDescription;
+        this.task_description = taskDescription;
     }
 
     public boolean isComplete() {
-        return isComplete;
+        return complete;
     }
 
     public void setComplete(boolean complete) {
-        isComplete = complete;
+        this.complete = complete;
     }
 
-    public Date getCreatedAt() {
-        return createdAt;
+    public Timestamp getCreatedAt() {
+        return created_at;
     }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
+    public void setCreatedAt(Timestamp createdAt) {
+        this.created_at = createdAt;
     }
 
     @Override
     public String toString() {
         return "Task{" +
-                "taskId=" + taskId +
-                ", taskDescription='" + taskDescription + '\'' +
-                ", isComplete=" + isComplete +
-                ", createdAt=" + createdAt +
+                "taskId=" + task_id +
+                ", taskDescription='" + task_description + '\'' +
+                ", isComplete=" + complete +
+                ", createdAt=" + created_at +
                 '}';
     }
 }
