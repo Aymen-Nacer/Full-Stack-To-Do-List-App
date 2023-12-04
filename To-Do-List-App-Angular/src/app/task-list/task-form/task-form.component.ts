40556b6646b6f48edb34ad60ba3e9e010da955c7
Aymen Nacer <aymen@nacer.com>
2023-12-04 14:39:29 +0300

    Trigger reload service

--- To-Do-List-App-Angular/src/app/task-list/task-form/task-form.component.ts
+++ To-Do-List-App-Angular/src/app/task-list/task-form/task-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/data.service';
-import { Task } from 'src/app/task';
+import { ReloadService } from 'src/app/reload.service';
 
 @Component({
   selector: 'app-task-form',
@@ -8,13 +8,25 @@ import { Task } from 'src/app/task';
   styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent {
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private reloadService: ReloadService
+  ) {}
 
   onTaskAdded(inputElement: HTMLInputElement): void {
     const TaskDescription = inputElement.value;
+    inputElement.value = '';
     this.dataService.saveTask(TaskDescription).subscribe({
-      next: (response: any) => {}, // completeHandler
-      error: (err: any) => {}, // errorHandler
+      next: (response: any) => {
+        this.reloadParent();
+      }, // completeHandler
+      error: (err: any) => {
+        console.log(err);
+      }, // errorHandler
     });
+  }
+
+  reloadParent() {
+    this.reloadService.triggerReload();
   }
 }
