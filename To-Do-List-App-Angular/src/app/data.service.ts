a8b25951c60bc2379d88527556be4170c3260067
Aymen Nacer <aymen@nacer.com>
2023-12-04 13:02:20 +0300

    Add more configuration to the api url

--- To-Do-List-App-Angular/src/app/data.service.ts
+++ To-Do-List-App-Angular/src/app/data.service.ts
@@ -13,20 +13,20 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl + '/todos');
+    return this.http.get<Task[]>(this.apiUrl + '/api/todos');
   }
 
   saveTask(taskDescription: string): Observable<any> {
-    return this.http.post<any>(this.apiUrl + '/todos', taskDescription);
+    return this.http.post<any>(this.apiUrl + '/api/todos', taskDescription);
   }
 
   updateTask(taskId: number, updatedData: Task): Observable<any> {
-    const url = `${this.apiUrl}/todos/${taskId}`;
+    const url = `${this.apiUrl}/api/todos/${taskId}`;
     return this.http.put(url, updatedData);
   }
 
   deleteTask(taskId: number): Observable<any> {
-    const url = `${this.apiUrl}/todos/${taskId}`;
+    const url = `${this.apiUrl}/api/todos/${taskId}`;
     return this.http.delete(url);
   }
 }
